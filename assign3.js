dba7f9b51b4c71e0bc01b41c2454946365181205
Shree74 <sandip.karekar@mindstix.com>
2015-02-06 18:42:09 +0530

    Added code for displaying the image to the client in the file. Added code for checking the error of "Bad request" (URL module).

--- assign3.js
+++ assign3.js
@@ -1,11 +1,34 @@
 var sys = require('sys');
 var http = require('http');
+var url = require('url');
 
 http.createServer(function (req, res){
   var parts = req.url.split('/');
+  var parsed = url.parse(req.url);
+
+  console.log(parts[1]);
   
-  res.writeHead(200, {'content-type': 'text/html'});
-  res.end('<h1>' + parts[1] + '</h1>');
+  var fs = require('fs');
+  fs.exists(parts[1], function (exists){
+    if(exists){
+      fs.readFile(parts[1], function (err, data) {
+        if (err) {
+          res.writeHead(500, {"Content-Type": "text/html"});
+          res.write("<h1>Internal server error</h1>");
+          res.end();
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write('<html><body><h1>' + parsed.pathname + '</h1><img src="data:image/jpeg;base64,')
+          res.write(new Buffer(data).toString('base64'));
+          res.end('"/></body></html>');
+        }
+      });
+    } else {
+      res.writeHead(404, {"Content-Type": "text/html"});
+      res.write("<h1>File not found " + parts[1] + "</h1>");
+      res.end();
+    }
+  });
 }).listen(3000, '127.0.0.1');
 
 sys.puts('Server running at http://127.0.0.1:3000');
